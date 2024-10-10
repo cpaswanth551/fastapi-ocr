3ff2fe1bdd8a964af3dd57102b6ff0513ef4e8c0
cpaswanthpalayad <cpaswanth551@gmail.com>
2024-10-10 18:17:19 +0530

    simple third party ocr

---
+++ main.py
@@ -0,0 +1,14 @@
+import shutil
+from fastapi import FastAPI, File, UploadFile
+import pytesseract
+
+
+app = FastAPI()
+
+
+@app.post("/ocr")
+def ocr(image: UploadFile = File(...)):
+    filepath = "txtFile"
+    with open(filepath, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    return pytesseract.image_to_string(filepath, lang="eng")
